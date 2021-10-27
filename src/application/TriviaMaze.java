97b73acd1d7e5e9c19484419cb1f33f2b52e883a
Codi Chun <codichun@uw.edu>
2021-10-27 11:00:24 -0700

    added basic frame

---
+++ src/application/TriviaMaze.java
@@ -0,0 +1,13 @@
+package application;
+
+import view.TriviaMazeGUI;
+
+public class TriviaMaze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TriviaMazeGUI();
+
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module TriviaMaze {
+	requires java.desktop;
 }
