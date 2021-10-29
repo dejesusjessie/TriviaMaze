c7d102d208d411c6ead9dac0b2fe2eeee790363f
Codi Chun <codichun@uw.edu>
2021-10-28 22:54:15 -0700

    added the frame with menu and three empty panels' classes

---
+++ src/view/RoomPanel.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class RoomPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/view/TriviaMazeGUI.java
+++ src/view/TriviaMazeGUI.java
@@ -7,7 +7,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 /**
  * A basic GUI for the game.
@@ -16,6 +19,11 @@ import javax.swing.JPanel;
  */
 public class TriviaMazeGUI extends JFrame implements ActionListener {
 	
+	/**
+	 * The serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * The width of the frame.
 	 */
@@ -29,37 +37,171 @@ public class TriviaMazeGUI extends JFrame implements ActionListener {
 	/**
 	 * The title of the frame.
 	 */
-	private static final String MY_TITLE = "Trivia Maze";
+	private static final String MY_TITLE = "Trivia Maze 1.0";
+	
+	/**
+	 * The basic Frame
+	 */
+	public static final JFrame myWindow  = new JFrame();
+	
+	/**
+	 * The maze panel
+	 */
+	private static final MazePanel myMazePanel = new MazePanel();
+	
+	
+	/**
+	 * The File menu at menu bar.
+	 */
+	private static final JMenu myFileMenu = new JMenu("File");
+	
+	/**
+	 * The Help menu at menu bar.
+	 */
+	private static final JMenu helpMenu = new JMenu("Help");
+	
+	
 	
 	/**
 	 * Constructor.
 	 * Constructs a new TriviaMazeGui.
 	 */
 	public TriviaMazeGUI() {
-		BasicPanel();
+		basicWindow();
 	}
 	
 	/**
 	 * Creating the basic frame and panel.
 	 */
-	public void BasicPanel() {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(MY_HEIGHT, MY_WIDTH, MY_HEIGHT, MY_WIDTH));
-		panel.setLayout(new GridLayout());
+	public void basicWindow() {
 		
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle(MY_TITLE);
-		frame.pack();
-		frame.setVisible(true);
+		//basic setting for the window.
+		myMazePanel.setBorder(BorderFactory.createEmptyBorder(MY_HEIGHT, MY_WIDTH, MY_HEIGHT, MY_WIDTH));
+		myMazePanel.setLayout(new GridLayout());
+		myWindow.add(myMazePanel, BorderLayout.CENTER);
+		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//setting the title of the frame.
+		myWindow.setTitle(MY_TITLE);
+
+		
+		//adding menu bar
+		JMenuBar menuBar = new JMenuBar();
+		myWindow.setJMenuBar(menuBar);
+		
+		// adding File menu
+		menuBar.add(myFileMenu);
+		addSaveMenu();
+		addLoadMenu();
+		addExitMenu();
+		
+		//adding Help menu 
+		menuBar.add(helpMenu);
+		addAboutMenu();
+		addInstructionMenu();
+		addCheatsMenu();
+		
+		//packing and showing the frame
+		myWindow.pack();
+		myWindow.setVisible(true);
+	} 
+
+	/**
+	 * Adding the exit menu and the action listener.
+	 */
+	private void addExitMenu() {
+		JMenuItem exitMenu = new JMenuItem("Exit");
+		myFileMenu.add(exitMenu);
+		exitMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 	}
 	
+	/**
+	 * Adding the save menu and the action listener.
+	 */
+	private void addSaveMenu() {
+		JMenuItem saveMenu = new JMenuItem("Save Game");
+		myFileMenu.add(saveMenu);
+	}
 	
+	/**
+	 * Adding the load menu and the action listener.
+	 */
+	private void addLoadMenu() {
+		JMenuItem loadMenu = new JMenuItem("Load Game");
+		myFileMenu.add(loadMenu);
+	}
+
+	/**
+	 * Adding the about menu and the action listener.
+	 */
+	private void addAboutMenu() {
+		JMenuItem aboutMenu = new JMenuItem("About");
+		helpMenu.add(aboutMenu);
+		aboutMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Trivia Maze\nVersion 1.0\n");
+			}
+		});
+		
+	}
+	
+	/**
+	 * Adding the instruction menu and the action listener.
+	 */
+	private void addInstructionMenu() {
+		JMenuItem instructionMenu = new JMenuItem("Game Play Instructions");
+		helpMenu.add(instructionMenu);
+		instructionMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "1. For moving to another room, you need to open a door."
+						+"\n2. You need to selecte the correct answer to open a door, otherwise, the door closes forever.");
+			}
+		});
+	}
+	
+	/**
+	 * Adding the cheats menu and the action listener.
+	 */
+	private void addCheatsMenu() {
+		JMenu cheatsMenu = new JMenu("Cheats");
+		JMenuItem doorKeyMenu = new JMenuItem("Door Key");
+		JMenuItem hinsAngelMenu = new JMenuItem("Hins Angel");
+		cheatsMenu.add(doorKeyMenu);
+		cheatsMenu.add(hinsAngelMenu);
+		helpMenu.add(cheatsMenu);
+		
+		doorKeyMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "needs to be edied");
+			}
+		});
+		
+		hinsAngelMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "The choice is wrong! (needs to be edied)");
+			}
+		});
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
 	}
+	
 
 }
