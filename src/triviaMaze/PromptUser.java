2c992d9ca37be11307aef3a81670b6aab4253950
akannika <akannika@uw.edu>
2021-11-20 13:39:38 -0800

    Create the username method and and the triviaMazeTester

--- src/triviaMaze/PromptUser.java
+++ src/triviaMaze/PromptUser.java
@@ -1,4 +1,32 @@
 package triviaMaze;
 
+import java.util.Scanner;
+
 public class PromptUser {
+
+    private Scanner myUserInput; // use to take all input from the user
+
+    // The PromptUser constructor
+    public PromptUser(){
+        this.myUserInput = new Scanner(System.in); //get the input from the user
+    }
+
+    //Return the username
+    public String userName() {
+        String myUserName = ""; //Initial to empty string
+
+        System.out.println("Please enter your username: ");
+        myUserName = myUserInput.nextLine();
+
+        //The player have to create the username.
+        while (myUserName.isEmpty()){
+            System.out.println("You did not enter your username, please enter your username:");
+            myUserName = myUserInput.nextLine();
+        }
+
+        System.out.println("Welcome " + myUserName +  " to the Husky Trivia Maze Game.");
+
+        return myUserName;
+    }
+
 }
